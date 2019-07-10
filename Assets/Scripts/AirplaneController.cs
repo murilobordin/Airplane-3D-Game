30087be7cf7ae685e9eea791d17550d4d2d74cf1
Murilo Bordin <murilobordin@gmail.com>
2019-07-10 18:17:56 -0300

    first commit

---
+++ Assets/Scripts/AirplaneController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirplaneController : MonoBehaviour
+{
+    private Rigidbody rBody;
+    private Quaternion airplaneRot;
+
+    [SerializeField]
+    private float vertRot;
+    [SerializeField]
+    private float horiRot;
+    [SerializeField]
+    private float angularSpeed = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horiRot = HorizontalInput(horiRot);
+        vertRot = VerticalInput(vertRot);
+
+        MoveAirplane(horiRot, vertRot); 
+    }
+
+    void MoveAirplane(float horizontal, float vertical)
+    {
+        transform.Translate(Vector3.right * Time.deltaTime);
+        transform.localEulerAngles = new Vector3(horizontal, 0f, vertical);
+    }
+
+    private float HorizontalInput(float horiInput)
+    {
+        if (Input.GetKey(KeyCode.A))
+            horiInput+=Time.deltaTime*angularSpeed;
+        else if (Input.GetKey(KeyCode.D))
+            horiInput-=Time.deltaTime*angularSpeed;
+
+        return horiInput;
+    }
+
+    private float VerticalInput(float vertInput)
+    {
+        if (Input.GetKey(KeyCode.W))
+            vertInput-=Time.deltaTime*angularSpeed;
+        else if (Input.GetKey(KeyCode.S))
+            vertInput+=Time.deltaTime*angularSpeed;
+
+        return vertInput;
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform camSupport, airplane;
+    private Vector3 prevPosition, newPosition;
+    private float distance = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        airplane = GameObject.FindWithTag("Airplane").transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera(airplane.position);
+    }
+
+    void MoveCamera(Vector3 target)
+    {
+        transform.LookAt(target);
+        prevPosition = airplane.localPosition;
+        prevPosition.x -= distance;
+        transform.position = prevPosition;
+    }
+}
